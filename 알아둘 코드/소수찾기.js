a67b3d5361aea6b45e88b61f66068084afc27ec2
juyeong-chungbuk <happyu9986@gmail.com>
2022-01-01 21:46:49 +0900

    소수찾는 2가지 방법

--- 알아둘 코드/소수찾기.js
+++ 알아둘 코드/소수찾기.js
@@ -1,8 +1,8 @@
 // 방법1
 function isPrime(num) {
     for(let i = 2; num > i; i++) {
-    if(num % i === 0)   //이 부분에서 num이 다른 수로 나눠떨어진다면 소수가 아님
-      return false;
+        if(num % i === 0)   //이 부분에서 num이 다른 수로 나눠떨어진다면 소수가 아님
+          return false;
     }
    return num > 1;
 }
