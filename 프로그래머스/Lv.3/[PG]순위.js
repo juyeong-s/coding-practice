5ec46cf1dfcb2c08fb46c49e54e9fedb4a33f70b
juyeong-chungbuk <happyu9986@gmail.com>
2022-01-30 02:02:50 +0900

    Lv.3 - 순위 : 그래프 - floyd-warshall

--- 프로그래머스/Lv.3/[PG]순위.js
+++ 프로그래머스/Lv.3/[PG]순위.js
@@ -30,3 +30,29 @@ function solution(n, results) {
     
     return answer;
 }
+
+// floyd-Warshall 방법
+function solution(n, results) {
+    let answer = 0;
+    let graph = Array.from({ length: n + 1 }, () => new Array(n + 1).fill(Infinity));
+
+    results.forEach((edge, idx) => {
+        const [winner, loser] = edge;
+        graph[winner][loser] = 1;
+        graph[loser][winner] = -1;
+        graph[winner][winner] = 0;
+        graph[loser][loser] = 0;
+    });
+    
+    for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= n; j++){
+            for(let k = 1; k <= n; k++){
+                if(graph[j][i] === 1 && graph[i][k] === 1) graph[j][k] = 1;
+                if(graph[j][i] === -1 && graph[i][k] === -1) graph[j][k] = -1;
+            }
+        }
+    }
+    
+    for(let i = 1; i <= n; i++) if(!graph[i].slice(1).includes(Infinity)) answer++;
+    return answer;
+}
