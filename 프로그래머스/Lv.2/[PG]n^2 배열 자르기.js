d3b877e9672d88fd4dc2cc9ed5da84f1c7c683cb
juyeong-chungbuk <happyu9986@gmail.com>
2022-02-03 22:32:12 +0900

    Lv.2 - n^2배열 자르기 : 월간 코드 챌린지 시즌3

---
+++ 프로그래머스/Lv.2/[PG]n^2 배열 자르기.js
@@ -0,0 +1,21 @@
+function solution(n, left, right) {
+    const arr = [];
+
+    for(let i = left; i <= right; i++){
+        arr.push(Math.max(Math.floor(i / n), i % n) + 1);
+    }
+    return arr;
+}
+
+// 시간, 메모리 초과 코드
+function solution(n, left, right) {
+    const emptyArray = Array.from({ length: n }, () => Array(n).fill());
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j <= i; j++){
+            emptyArray[i][j] = i + 1;
+            emptyArray[j][i] = i + 1;
+        }
+    }
+    const arr = emptyArray.flat();
+    return arr.slice(left, right + 1);
+}
