bd052a3859aae6fa7e3220f3d9256bc024aa4e5f
juyeong-chungbuk <happyu9986@gmail.com>
2022-02-06 16:48:03 +0900

    Lv.2 - 가장 큰 정사각형 찾기 : 연습문제

--- 프로그래머스/Lv.2/[PG]가장 큰 정사각형 찾기.js
+++ 프로그래머스/Lv.2/[PG]가장 큰 정사각형 찾기.js
@@ -12,10 +12,14 @@ function solution(board)
                 const up = board[i - 1][j];
                 const left = board[i][j - 1];
                 const cross = board[i - 1][j - 1];
-                board[i][j] = Math.min(up, left, cross) + 1;
+                board[i][j] = Math.min(up, left, cross) + 1; // 전부 1이면 11
+                                                             //          11 이니까 +1 해서 길이가 2가 됨을 업데이트 해줌.
                 answer = Math.max(answer, board[i][j]);
             }
         }
     }
     return answer * answer;
 }
+
+// 만약, 전부 2로 업데이트되면 22
+//                       21 가 되고, 결국 2+1이 돼서 길이는 3으로 업뎃됨
