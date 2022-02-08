aa0af7ae563d7fe5ce94c3f341ed611f1997db76
juyeong-chungbuk <happyu9986@gmail.com>
2022-02-09 00:05:10 +0900

    Lv.2 - 모음사전 : 위클리 챌린지 - 풀이 2추가

--- 프로그래머스/Lv.2/[PG]모음사전.js
+++ 프로그래머스/Lv.2/[PG]모음사전.js
@@ -1,4 +1,4 @@
-function solution(word) {
+function solution(word) {   // 풀이 1
     const LEN = 5;
     let allCase = 0;
     for(let i = 1; i <= LEN; i++) allCase += Math.pow(LEN, i);
@@ -15,6 +15,25 @@ function solution(word) {
             answer += Math.floor((allCase / Math.pow(LEN, i + 1)) * 3 + 1);
         } else if(word[i] === 'U'){
             answer += Math.floor((allCase / Math.pow(LEN, i + 1)) * 4 + 1);
+        }
+    }
+    return answer;
+}
+
+function solution(word) {   // 풀이 2
+    const LEN = 5;
+    let answer = 0;
+    for(let i = 0; i < word.length; i++){
+        if(word[i] === 'A'){
+            answer += 1;
+        } else if(word[i] === 'E'){
+            answer += Math.floor((1 * Math.pow(LEN, LEN - i) - 1) / 4 + 1);
+        } else if(word[i] === 'I'){
+            answer += Math.floor((2 * Math.pow(LEN, LEN - i) - 1) / 4 + 1);
+        } else if(word[i] === 'O'){
+            answer += Math.floor((3 * Math.pow(LEN, LEN - i) - 1) / 4 + 1);
+        } else if(word[i] === 'U'){
+            answer += Math.floor((4 * Math.pow(LEN, LEN - i) - 1) / 4 + 1);
         }
     }
     return answer;
