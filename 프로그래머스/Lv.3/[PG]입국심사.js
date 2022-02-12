7e76fe3bd8dba1b32567aeeff7ae8948cf641185
juyeong-chungbuk <happyu9986@gmail.com>
2022-02-12 21:56:14 +0900

    Lv.3 - 입국심사 : 이분탐색

---
+++ 프로그래머스/Lv.3/[PG]입국심사.js
@@ -0,0 +1,13 @@
+function solution(n, times) {
+    times.sort((a, b) => a - b); 
+    let min = times[0];
+    let max = n * times[times.length -1];
+
+    while(min <= max){
+        let mid = Math.floor((min + max) / 2);
+        let count = times.reduce((acc, curr) => acc + Math.floor(mid / curr), 0); // 한 사람당 몇명 할 수 있는지
+        if (count >= n) max = mid - 1;
+        else min = mid + 1;
+    }
+    return min;
+}
