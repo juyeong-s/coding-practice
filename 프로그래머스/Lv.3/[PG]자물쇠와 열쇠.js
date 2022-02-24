536420b29a5bcbc08e126c9dbcd515403a2a2d15
juyeong-chungbuk <happyu9986@gmail.com>
2022-02-24 19:18:28 +0900

    Lv.3 - 자물쇠와 열쇠 : 2020 KAKAO

--- 프로그래머스/Lv.3/[PG]자물쇠와 열쇠.js
+++ 프로그래머스/Lv.3/[PG]자물쇠와 열쇠.js
@@ -2,7 +2,7 @@ function solution(key, lock) {
     const N = key.length;
     const M = lock.length;
     const keyCol = key[0].length;
-    const board = Array.from({ length : M * 3 }, () => Array(M * 3).fill(null));
+    const board = Array.from({ length : M * 3 }, () => Array(M * 3).fill());
     
     for(let i = M; i < M * 2; i++) {
         for(let j = M; j < M * 2; j++) {
@@ -23,7 +23,7 @@ function solution(key, lock) {
         const row = board.length, col = board[0].length;
         for(let i = 0; i <= row - N; i++){
             for(let j = 0; j <= col - keyCol; j++){
-                const copyLock = board.map((el) => el.slice()); // null은 빼고 자름
+                const copyLock = board.map((el) => el.slice());
                 for(let k = 0; k < N; k++){
                     for(let z = 0; z < N; z++){
                         if(copyLock[i + k][j + z] === 1 && key[k][z] === 1) copyLock[i + k][j + z] = 2;
@@ -37,7 +37,7 @@ function solution(key, lock) {
     }
     
     function rotatekey(){ // 90도 회전
-        let rotated = Array.from({ length: N }, () => Array(N).fill(null));
+        let rotated = Array.from({ length: N }, () => Array(N).fill());
         for(let i = 0; i < N; ++i){
             for(let j = 0; j < N; ++j){
                 rotated[i][j] = key[N - j - 1][i];
