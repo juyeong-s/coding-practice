8b41d79663fee8d74ce433e3f3acebfc6c0ba7fc
juyeong-chungbuk <happyu9986@gmail.com>
2022-03-01 02:18:48 +0900

    Lv.3 - 보석쇼핑 : 2020 카카오 인턴십

--- 프로그래머스/Lv.3/[PG]보석쇼핑.js
+++ 프로그래머스/Lv.3/[PG]보석쇼핑.js
@@ -6,14 +6,14 @@ function solution(gems) {   // 정확성 success, 효율성 success
 
     for(let i = 0; i < len; i++) {
         const gem = gems[i];
-        gemsMap.delete(gem);
-        gemsMap.set(gem, i);
+        gemsMap.delete(gem);    // 중복된게 들어온다면 빼주고
+        gemsMap.set(gem, i);    // 새로운 인덱스로 반영
 
         if (gemsMap.size === category) {
             const start = gemsMap.values().next().value;
-            if (result[1] - result[0] > i - start)
+            if (result[1] - result[0] > i - start){
                 result = [start + 1, i + 1];
-            
+            }
             gemsMap.delete(gems[start]);
         }
     }
@@ -38,4 +38,28 @@ function solution(gems) {   // 정확성 success, 효율성 fail
         }
     }
     return result;
+}
+
+// 다른 사람 풀이
+function solution(gems) {
+    const gemVarietyCounts = new Set(gems).size;
+
+    const gemMap = new Map();
+    const gemLengths = [];
+    gems.forEach((gem,i) => {
+        gemMap.delete(gem);
+        gemMap.set(gem,i);
+        if(gemMap.size === gemVarietyCounts) {
+            gemLengths.push([gemMap.values().next().value + 1, i + 1 ])
+        }
+    })
+
+    gemLengths.sort((a, b) => {
+        if ((a[1] - a[0]) === (b[1] - b[0])) {
+            return a[1] - b[1];
+        }
+        return (a[1] - a[0]) - (b[1] - b[0])
+    });
+
+    return gemLengths[0]
 }
