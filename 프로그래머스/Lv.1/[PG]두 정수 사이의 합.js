30f5eb7d0ecf5a82b0edd0468d72f4ffe4582e7a
juyeong-chungbuk <happyu9986@gmail.com>
2022-03-05 13:35:08 +0900

    Lv.1 - 두 정수 사이의 합 : 연습문제

--- 프로그래머스/Lv.1/[PG]두 정수 사이의 합.js
+++ 프로그래머스/Lv.1/[PG]두 정수 사이의 합.js
@@ -1,3 +1,22 @@
+function solution(a, b) {
+    let sum = 0;
+    if(a > b){
+        let temp = 0;
+        temp = a;function solution(a, b){
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) sum += i;
+    return sum;
+}
+        a = b;
+        b = temp;
+    }
+    for(let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+// 다른 사람 풀이
 function solution(a, b){
     let sum = 0;
     for (let i = Math.min(a, b); i <= Math.max(a, b); i++) sum += i;
