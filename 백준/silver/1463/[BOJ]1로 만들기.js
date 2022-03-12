7a953466f8df043164caa4de27be13791312d7a4
juyeong-chungbuk <happyu9986@gmail.com>
2022-03-12 20:10:43 +0900

    silver3 - 1로 만들기

--- 백준/silver/1463/[BOJ]1로 만들기.js
+++ 백준/silver/1463/[BOJ]1로 만들기.js
@@ -13,4 +13,35 @@ function solve(N){
 const filePath = process.platform === 'linux'? '/dev/stdin' : '백준/silver/1463/testcase.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const N = +input[0];
+
+// 다른사람 풀이
+const numSet = new Set();
+let count = 0;
+let queue = [+input];
+
+function solve(x) {
+    while(queue.length) {
+        const newQueue = [];
+        for(const num of queue) {
+            if(num === 1) {
+                return count;
+            }
+            if(!(num % 3) && !numSet.has(num / 3)) {
+                newQueue.push(num / 3);
+                numSet.add(num / 3);
+            }
+            if(!(num % 2) && !numSet.has(num / 2)) {
+                newQueue.push(num / 2);
+                numSet.add(num / 2);
+            }
+            if(!numSet.has(num - 1)) {
+                newQueue.push(num - 1);
+                numSet.add(num / 3);
+            }
+        }
+        queue = newQueue;
+        count++;
+    }
+}
+
 console.log(solve(N));
