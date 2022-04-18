1c84f62d1a984179ae6fc0c2ad1abcb49252f008
juyeong-chungbuk <happyu9986@gmail.com>
2022-04-19 01:40:14 +0900

    [BOJ-7569] gold5 : 토마토

--- 백준/gold/7569/[BOJ]토마토.js
+++ 백준/gold/7569/[BOJ]토마토.js
@@ -1,65 +1,59 @@
 function solve() {
-  console.log(m, n, h, tomatoBoard);
   const dx = [-1, 1, 0, 0, 0, 0];
   const dy = [0, 0, -1, 1, 0, 0];
   const dz = [0, 0, 0, 0, -1, 1];
 
-  function checkRipe() {
-    for (let i = 0; i < h; i++) {
-      for (let j = 0; j < n; j++) {
-        for (let k = 0; k < m; k++) {
-          if (tomatoBoard[i][j][k] === 0) return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  function isValid(x, y, z) {
+  function isValid(z, x, y) {
     return x >= 0 && x < n && y >= 0 && y < m && z >= 0 && z < h;
   }
 
   function bfs() {
-    const queue = [];
+    let number = 0;
 
+    const queue = [];
     for (let i = 0; i < h; i++) {
       for (let j = 0; j < n; j++) {
         for (let k = 0; k < m; k++) {
-          if (tomatoBoard[i][j][k] === 1) {
+          if (tomatoBoard[i][j][k] === "1") {
             queue.push([i, j, k]);
           }
         }
       }
     }
 
-    let answer = 0;
-    while (queue.length) {
-      const len = queue.length;
-      answer++;
+    while (number !== queue.length) {
+      const [currZ, currX, currY] = queue[number];
 
-      for (let i = 0; i < len; i++) {
-        const [currZ, currX, currY] = queue.shift();
-        for (let j = 0; j < 6; j++) {
-          const [moveX, moveY, moveZ] = [
-            currX + dx[i],
-            currY + dy[i],
-            currZ + dz[i],
-          ];
+      for (let i = 0; i < 6; i++) {
+        const moveZ = currZ + dz[i];
+        const moveX = currX + dx[i];
+        const moveY = currY + dy[i];
 
-          if (
-            isValid(moveX, moveY, moveZ) &&
-            tomatoBoard[moveZ][moveX][moveY] === 0
-          ) {
-            queue.push([moveZ, moveX, moveY]);
-            tomatoBoard[moveZ][moveX][moveY] = 1;
-          }
+        if (
+          isValid(moveZ, moveX, moveY) &&
+          tomatoBoard[moveZ][moveX][moveY] === "0"
+        ) {
+          queue.push([moveZ, moveX, moveY]);
+          tomatoBoard[moveZ][moveX][moveY] =
+            +tomatoBoard[currZ][currX][currY] + 1 + "";
         }
       }
+      number++;
     }
-    return checkRipe() ? answer - 1 : -1;
   }
 
-  return bfs();
+  bfs();
+
+  let answer = 0;
+  for (const box of tomatoBoard) {
+    for (const row of box) {
+      for (const value of row) {
+        if (value === "0") return -1;
+        answer = Math.max(answer, value);
+      }
+    }
+  }
+  return answer === 1 ? 0 : answer - 1;
 }
 
 const filePath =
@@ -68,9 +62,10 @@ const input = require("fs")
   .readFileSync(filePath)
   .toString()
   .trim()
-  .split("\n");
-const [m, n, h] = input[0].split(" ").map((el) => +el);
-const box = input.slice(1).map((row) => row.split(" ").map((el) => +el));
+  .split("\n")
+  .map((row) => row.split(" "));
+const [m, n, h] = input[0].map((el) => +el);
+const box = input.slice(1);
 const tomatoBoard = [];
 for (let i = 0; i < n * h; i += n) {
   tomatoBoard.push(box.slice(i, i + n));
