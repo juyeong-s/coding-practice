00cbca1a29f09fcd0db7225e0a730947f78fe35c
juyeong-chungbuk <happyu9986@gmail.com>
2022-05-30 13:18:30 +0900

    [BOJ-1747] silver1 : 소수&팰린드롬

---
+++ 백준/silver/1747/[BOJ]소수&팰린드롬.js
@@ -0,0 +1,25 @@
+function solve() {
+  function isPrime(num) {
+    for (let i = 2; i < Math.sqrt(num) + 1; i++)
+      if (num % i === 0) return false;
+    return true;
+  }
+
+  while (true) {
+    const str = n + "";
+    if (str === str.split("").reverse().join("") && isPrime(n)) {
+      return n;
+    }
+    n++;
+  }
+}
+
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "백준/silver/1747/testcase.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+let n = +input[0];
+console.log(solve());
