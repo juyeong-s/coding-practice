3c0c67f5ee91766cd6889cb3b6225273a17c9a50
juyeong-chungbuk <happyu9986@gmail.com>
2022-05-30 13:26:24 +0900

    [BOJ-1747] silver1 : 소수&팰린드롬

--- 백준/silver/1747/[BOJ]소수&팰린드롬.js
+++ 백준/silver/1747/[BOJ]소수&팰린드롬.js
@@ -1,5 +1,7 @@
 function solve() {
   function isPrime(num) {
+    if (num === 1) return false;
+    if (num === 2) return true;
     for (let i = 2; i < Math.sqrt(num) + 1; i++)
       if (num % i === 0) return false;
     return true;
