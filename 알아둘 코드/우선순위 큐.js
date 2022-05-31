bd557977a3f51f4a08ed230f1f379096f9285be8
juyeong-chungbuk <happyu9986@gmail.com>
2022-05-31 10:56:18 +0900

    [common] 우선순위 큐

---
+++ 알아둘 코드/우선순위 큐.js
@@ -0,0 +1,88 @@
+class Heap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getLeftChildIndex = (parentIndex) => parentIndex * 2 + 1;
+  getRightChildIndex = (parentIndex) => parentIndex * 2 + 2;
+  getParentIndex = (childIndex) => Math.floor(childIndex - 1);
+
+  peek = () => this.heap[0];
+
+  insert = (key, value) => {
+    const node = { key, value };
+    this.heap.push(node);
+    this.heapifyUp();
+  };
+
+  heapifyUp = () => {
+    let index = this.heap.length - 1;
+    const lastInsertedNode = this.heap[index];
+
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index);
+
+      if (this.heap[parentIndex].key > lastInsertedNode.key) {
+        this.heap[index] = this.heap[parentIndex];
+        index = parentIndex;
+      } else break;
+    }
+
+    this.heap[index] = lastInsertedNode;
+  };
+
+  remove = () => {
+    const count = this.heap.length;
+    const rootNode = this.heap[0];
+
+    if (count <= 0) return undefined;
+    if (count === 1) this.heap = [];
+    else {
+      this.heap[0] = this.heap.pop();
+      this.heapifyDown();
+    }
+
+    return rootNode;
+  };
+
+  heapifyDown = () => {
+    let index = 0;
+    const count = this.heap.length;
+    const rootNode = this.heap[index];
+
+    while (this.getLeftChildIndex(index) < count) {
+      const leftChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+
+      const smallerChildIndex =
+        rightChildIndex < count &&
+        this.heap[rightChildIndex].key < this.heap[leftChildIndex].key
+          ? rightChildIndex
+          : leftChildIndex;
+
+      if (this.heap[smallerChildIndex].key <= rootNode.key) {
+        this.heap[index] = this.heap[smallerChildIndex];
+        index = smallerChildIndex;
+      } else break;
+    }
+
+    this.heap[index] = rootNode;
+  };
+}
+
+class PriorityQueue extends Heap {
+  constructor() {
+    super();
+  }
+
+  enqueue = (priority, value) => this.insert(priority, value);
+  dequeue = () => this.remove();
+  isEmpty = () => this.heap.length <= 0;
+}
+
+const pq = new PriorityQueue();
+pq.enqueue(0, 1);
+pq.enqueue(5, 2);
+pq.enqueue(1, 3);
+pq.enqueue(10, 4);
+console.log(pq.heap);
