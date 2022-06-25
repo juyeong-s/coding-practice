9c49891c27f5d0a872767769981f63b49f53c94f
juyeong-chungbuk <happyu9986@gmail.com>
2022-06-25 17:10:58 +0900

    [BOJ-16198] silver1 : 에너지 모으기

---
+++ 백준/silver/16198/[BOJ]에너지 모으기.js
@@ -0,0 +1,38 @@
+function solve() {
+  let answer = 0;
+  function deleteMarble(totalEnergy, marble) {
+    const len = marble.length;
+    if (len === 2) {
+      answer = Math.max(answer, totalEnergy);
+      return;
+    }
+
+    for (let i = 1; i <= len - 2; i++) {
+      deleteMarble(
+        totalEnergy + marble[i - 1] * marble[i + 1],
+        marble.filter((_, index) => index !== i)
+      );
+    }
+  }
+
+  for (let i = 1; i <= n - 2; i++) {
+    deleteMarble(
+      marble[i - 1] * marble[i + 1],
+      marble.filter((_, index) => index !== i)
+    );
+  }
+  return answer;
+}
+
+const filePath =
+  process.platform === "linux"
+    ? "/dev/stdin"
+    : "백준/silver/16198/testcase.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+const n = +input[0];
+const marble = input[1].split(" ").map((num) => +num);
+console.log(solve());
