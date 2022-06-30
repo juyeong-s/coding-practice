0aa788885a2bb6c48f399292f45195446854f50c
juyeong-chungbuk <happyu9986@gmail.com>
2022-07-01 00:43:48 +0900

    [PG] resolve: 후보키

--- 프로그래머스/Lv.2/[PG]후보키.js
+++ 프로그래머스/Lv.2/[PG]후보키.js
@@ -1,47 +1,66 @@
-function getCombination(arr, num){
-    const result = [];
-    if(num === 1) return arr.map((v) => [v]);
-    
-    arr.forEach((fixed, idx, origin) => {
-        const rest = origin.slice(idx + 1);
-        const combinations = getCombination(rest, num - 1);
-        const attached = combinations.map((combination) => [fixed, ...combination]);
-        result.push(...attached);
-    });
-    return result;
+function getCombination(arr, num) {
+  const result = [];
+  if (num === 1) return arr.map((v) => [v]);
+
+  arr.forEach((fixed, idx, origin) => {
+    const rest = origin.slice(idx + 1);
+    const combinations = getCombination(rest, num - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
+    result.push(...attached);
+  });
+  return result;
 }
 
-function isUnique(arr){
-    let originLen = arr.length;
-    const SetLen = new Set(arr).size;
-    return originLen === SetLen ? true : false
+function isUnique(arr) {
+  const afterLen = new Set(arr).size;
+  return arr.length === afterLen;
 }
 
 function solution(relation) {
-    const combination = [];
-    const unique = [];
-    const indexes = Array.from({ length: relation[0].length }, (_, i) => i);
+  const colLen = relation[0].length;
+  const indexes = Array.from({ length: colLen }, (_, index) => index);
+  const combination = [];
+  const unique = [];
 
-    for(let i = 1; i <= indexes.length; i++){
-        combination.push(getCombination(indexes, i));
-    }
-    // 유일성 검사
-    for(let i = 0; i < combination.length; i++){
-        const tuple = combination[i];
-        for(let j = 0; j < combination[i].length; j++){
-            if(!tuple.some((v) => unique.some((x) => x.includes(v))))
-                isUnique(relation.map((arr) => arr.filter((_, idx) => tuple[j].includes(idx)).join(''))) ? unique.push(tuple[j]) : null;
+  for (let i = 1; i <= indexes.length; i++) {
+    combination.push(getCombination(indexes, i));
+  }
+
+  // 유일성 검사
+  for (const tuple of combination) {
+    for (const col of tuple) {
+      if (!tuple.some((v) => unique.some((x) => x.includes(v)))) {
+        const combine = relation.map((row) =>
+          row.filter((_, idx) => col.includes(idx)).join("")
+        );
+
+        if (isUnique(combine)) {
+          unique.push(col);
         }
+      }
     }
-    // 최소성 검사
-    const answer = [];
-    for(const u of unique){
-        const sub = u;
-        let check = true;
-        for(const a of answer){
-            if(a.filter((v) => !sub.includes(v)).length === 0) check = false;
-        }
-        if(check) answer.push(sub);
+  }
+
+  // 최소성 검사
+  const answer = [];
+  for (const sub of unique) {
+    let check = true;
+    for (const candidate of answer) {
+      if (candidate.filter((col) => !sub.includes(col)).length === 0)
+        check = false;
     }
-    return answer.length;
+    if (check) answer.push(sub);
+  }
+  return answer.length;
 }
+
+console.log(
+  solution([
+    ["100", "ryan", "music", "2"],
+    ["200", "apeach", "math", "2"],
+    ["300", "tube", "computer", "3"],
+    ["400", "con", "computer", "4"],
+    ["500", "muzi", "music", "3"],
+    ["600", "apeach", "music", "2"],
+  ])
+);
