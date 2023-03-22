a41d5e7be7b9cba556f21d6fdd9f4145a751e082
juyeong-chungbuk <happyu9986@gmail.com>
2023-03-22 22:33:42 +0900

    [BOJ] resolve : 문자열 잘라내기 - solve2

--- 백준/gold/2866/[BOJ]문자열 잘라내기.js
+++ 백준/gold/2866/[BOJ]문자열 잘라내기.js
@@ -1,4 +1,4 @@
-function solve(r, c, table) {
+function solve1(r, c, table) {
   let count = 0;
   let start = 0;
   let end = r - 1;
@@ -33,8 +33,29 @@ function solve(r, c, table) {
   return count;
 }
 
+function solve2(r, c, table) {
+  const attachedStr = Array.from({ length: c }, () => "");
+
+  for (let i = 0; i < c; i++) {
+    for (let j = 1; j < r; j++) {
+      attachedStr[i] += table[j][i];
+    }
+  }
+
+  let start = 0;
+  let end = r - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    const set = new Set(attachedStr.map((el) => el.slice(mid)));
+    set.size === c ? (start = mid + 1) : (end = mid - 1);
+  }
+  return start;
+}
+
 const filePath = process.platform === "linux" ? "/dev/stdin" : "백준/gold/2866/testcase.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 const [r, c] = input[0].split(" ").map((el) => +el);
 const table = input.slice(1);
-console.log(solve(r, c, table));
+console.log(solve1(r, c, table));
+console.log(solve2(r, c, table));
