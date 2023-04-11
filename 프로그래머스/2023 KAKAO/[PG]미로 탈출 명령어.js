4940192f69dc7b8688b167efa77c241c766104cf
juyeong-chungbuk <happyu9986@gmail.com>
2023-04-11 22:20:45 +0900

    refactor: 다른 풀이법

--- 프로그래머스/2023 KAKAO/[PG]미로 탈출 명령어.js
+++ 프로그래머스/2023 KAKAO/[PG]미로 탈출 명령어.js
@@ -1,13 +1,13 @@
 function solution(n, m, x, y, r, c, k) {
-  const distDiff = k - (Math.abs(r - x) + Math.abs(c - y));
-  if (distDiff < 0 || distDiff % 2 !== 0) return "impossible";
-
   let answer = "z".repeat(k);
   const cmds = ["d", "l", "r", "u"];
   const dx = [1, 0, 0, -1];
   const dy = [0, -1, 1, 0];
 
+  const calcDist = (x, y) => Math.abs(r - x) + Math.abs(c - y);
+
   const isValid = (x, y) => x > 0 && x <= n && y > 0 && y <= m;
+
   const dfs = (x, y, cnt, cmd, dist) => {
     if (dist > k) return;
     if (cnt >= k && x === r && y === c) {
@@ -21,13 +21,59 @@ function solution(n, m, x, y, r, c, k) {
       const my = y + dy[i];
 
       if (isValid(mx, my)) {
-        dfs(mx, my, cnt + 1, cmd + cmds[i], Math.abs(r - mx) + Math.abs(c - my) + cnt + 1);
+        dfs(mx, my, cnt + 1, cmd + cmds[i], calcDist(mx, my) + cnt + 1);
       }
     }
   };
 
+  const distDiff = k - calcDist(x, y);
+  if (distDiff < 0 || distDiff % 2 !== 0) return "impossible";
+
   dfs(x, y, 0, "", k);
   return answer === "z".repeat(k) ? "impossible" : answer;
+}
+
+function solution2(n, m, x, y, r, c, k) {
+  let answer = "";
+  const cmds = ["d", "l", "r", "u"];
+  const dx = [1, 0, 0, -1];
+  const dy = [0, -1, 1, 0];
+
+  const calcDist = (x, y) => Math.abs(r - x) + Math.abs(c - y);
+
+  const isValid = (x, y) => x > 0 && x <= n && y > 0 && y <= m;
+
+  const move = (x, y) => {
+    for (let i = 0; i < 4; i++) {
+      const mx = x + dx[i];
+      const my = y + dy[i];
+
+      if (isValid(mx, my)) return i;
+    }
+  };
+
+  let distance = calcDist(x, y);
+  const diff = k - distance;
+  if (diff < 0 || diff % 2 !== 0) return "impossible";
+
+  while (distance < k) {
+    const idx = move(x, y);
+    if (idx === 0) x++;
+    else if (idx === 1) y--;
+    else if (idx === 2) y++;
+    else x--;
+
+    answer += cmds[idx];
+    distance = calcDist(x, y);
+    k--;
+  }
+
+  if (x < r) answer += "d".repeat(r - x);
+  if (y > c) answer += "l".repeat(y - c);
+  if (y < c) answer += "r".repeat(c - y);
+  if (x > r) answer += "u".repeat(x - r);
+
+  return answer;
 }
 
 console.log(solution(3, 4, 2, 3, 3, 1, 5));
