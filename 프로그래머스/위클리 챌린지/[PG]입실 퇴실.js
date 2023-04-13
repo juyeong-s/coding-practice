5839feb0dcc965c83f8407234d36f61144b5de63
juyeong-chungbuk <happyu9986@gmail.com>
2023-04-14 01:51:31 +0900

    [PG] 위클리 챌린지 - 입실 퇴실

---
+++ 프로그래머스/위클리 챌린지/[PG]입실 퇴실.js
@@ -0,0 +1,30 @@
+// Lv.2
+function solution(enter, leave) {
+  let answer = new Array(enter.length + 1).fill(0);
+  let room = [];
+
+  while (leave.length > 1) {
+    const inId = enter.shift();
+    if (inId) room.push(inId);
+    if (room.length > 1) {
+      room.forEach((id) => (answer[id] = id === inId ? room.length - 1 : answer[id] + 1));
+    }
+
+    if (inId === leave[0]) {
+      leave.shift();
+      room.pop();
+    }
+
+    while (room.includes(leave[0])) {
+      room = room.filter((id) => leave[0] !== id);
+      leave.shift();
+    }
+  }
+  return answer.slice(1);
+}
+
+console.log(solution([1, 3, 2], [1, 2, 3]));
+console.log(solution([1, 4, 2, 3], [2, 1, 3, 4]));
+console.log(solution([3, 2, 1], [2, 1, 3]));
+console.log(solution([3, 2, 1], [1, 3, 2]));
+console.log(solution([1, 4, 2, 3], [2, 1, 4, 3]));
